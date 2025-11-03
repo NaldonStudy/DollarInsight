93e279ba0689bb23884727cc5d690b3bb7eef0e8
KimDoHun <luckyboyhoon@naver.com>
2025-11-03 13:38:06 +0900

    Feat: Error 관련 통합
    - Error 관련 핸들링

--- backend/src/main/java/com/ssafy/b205/backend/support/error/ErrorCode.java
+++ backend/src/main/java/com/ssafy/b205/backend/support/error/ErrorCode.java
@@ -1,4 +1,18 @@
 package com.ssafy.b205.backend.support.error;
 
-public class ErrorCode {
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    BAD_REQUEST(HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED),
+    FORBIDDEN(HttpStatus.FORBIDDEN),
+    NOT_FOUND(HttpStatus.NOT_FOUND),
+    CONFLICT(HttpStatus.CONFLICT),
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR);
+
+    public final HttpStatus status;
+
+    ErrorCode(HttpStatus status) {
+        this.status = status;
+    }
 }
