d00261425972b537e63203388269b7fdfee9777c
KimDoHun <luckyboyhoon@naver.com>
2025-11-04 10:47:06 +0900

    Feat: Security관련 및 헤더

---
+++ backend/src/main/java/com/ssafy/b205/backend/infra/security/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.ssafy.b205.backend.infra.security;
+
+public final class SecurityConstants {
+    private SecurityConstants() {}
+    public static final String HEADER_AUTH   = "Authorization";
+    public static final String HEADER_DEVICE = "X-Device-Id";
+    public static final String BEARER_PREFIX = "Bearer ";
+}
