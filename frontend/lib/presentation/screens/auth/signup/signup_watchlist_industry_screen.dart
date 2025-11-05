58bd6bcd71e917ce5af9733f63a7de89536c2082
임주빈 <imjubing@gmail.com>
2025-11-05 10:33:48 +0900

    Add : 관심종목 기업선택, 관심종목 산업선택 스크린 추가 - 라우터링크 변경

---
+++ frontend/lib/presentation/screens/auth/signup/signup_watchlist_industry_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupWatchlistIndustryScreen extends StatelessWidget {
+  const SignupWatchlistIndustryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('관심종목산업선택')),
+      body: const Center(
+        child: Text('관심종목산업선택'),
+      ),
+    );
+  }
+}
