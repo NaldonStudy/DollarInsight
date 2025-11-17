243e53664384a119ea3ce20940f4569823182e5a
김준혁 <ricky1208@naver.com>
2025-11-17 10:16:27 +0900

    Add: 닉네임 변경 페이지 추가

--- frontend/lib/routes/app_router.dart
+++ frontend/lib/routes/app_router.dart
@@ -31,7 +31,7 @@ import '../presentation/screens/mypage/company_search_screen.dart';
 import '../presentation/screens/mypage/password_change_screen.dart';
 import '../presentation/screens/mypage/password_change_new_screen.dart';
 import '../presentation/screens/mypage/ai_friend_change_screen.dart';
-
+import '../presentation/screens/mypage/nickname_change.dart';
 import '../presentation/screens/test_chat_screen.dart';
 
 // Route Guards
@@ -126,6 +126,13 @@ class AppRouter {
           );
         },
       ),
+
+      // 닉네임 변경
+      GoRoute(
+        path: '/mypage/nickname-change',
+        builder: (context, state) => const NicknameChangeScreen(),
+      ),
+
 
       /// 회원가입 완료
       GoRoute(
